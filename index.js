6f349f3ebbeee14dba2f4d84c68e698cef128d0b
Fadi Alshareef <166608344+fadiwazir@users.noreply.github.com>
2025-09-29 08:07:49 +0200

    more simple

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 document.getElementById("btn").addEventListener("click", function () {
-    var valueHeight = document.getElementById("hight").value;
-    var valueWeight = document.getElementById("weight").value;
-    var height = Number(valueHeight);
-    var weight = Number(valueWeight);
-    var _BMI = weight / (height * height);
-    var roundBMI = Math.round(_BMI * 10) / 10;
-
-    if (roundBMI < 18.5) {
-        document.getElementById("BMI").innerHTML = `Underweight`;
-        document.getElementById("numberBMI").innerHTML = `${roundBMI}`;
-    } else if (roundBMI >= 18.5 && roundBMI <= 24.9) {
-        document.getElementById("BMI").innerHTML = `Normal`;
-        document.getElementById("numberBMI").innerHTML = `${roundBMI}`;
-    } else if (roundBMI >= 25 && roundBMI <= 29.9) {
-        document.getElementById("BMI").innerHTML = `Overweight`;
-        document.getElementById("numberBMI").innerHTML = `${roundBMI}`;
-    } else if (roundBMI >= 30 && roundBMI <= 34.9) {
-        document.getElementById("BMI").innerHTML = `Obese`;
-        document.getElementById("numberBMI").innerHTML = `${roundBMI}`;
-    } else {
-        document.getElementById("BMI").innerHTML = `Extremely obese`;
-        document.getElementById("numberBMI").innerHTML = `${roundBMI}`;
-    }
+  var valueHeight = document.getElementById("hight").value;
+  var valueWeight = document.getElementById("weight").value;
+  var height = Number(valueHeight);
+  var weight = Number(valueWeight);
+  var _BMI = weight / (height * height);
+  var roundBMI = Math.round(_BMI * 10) / 10;
+  //   more simple
+  var bmiMassage;
+  var bmiNum;
+  if (roundBMI < 18.5) {
+    bmiMassage = `Underweight`;
+    bmiNum = `${roundBMI}`;
+  } else if (roundBMI >= 18.5 && roundBMI <= 24.9) {
+    bmiMassage = `Normal`;
+    bmiNum = `${roundBMI}`;
+  } else if (roundBMI >= 25 && roundBMI <= 29.9) {
+    bmiMassage = `Overweight`;
+    bmiNum = `${roundBMI}`;
+  } else if (roundBMI >= 30 && roundBMI <= 34.9) {
+    bmiMassage = `Obese`;
+    bmiNum = ` ${roundBMI}`;
+  } else {
+    bmiMassage = `Extremely obese`;
+    bmiNum = ` ${roundBMI}`;
+  }
+  document.getElementById("BMI").innerHTML = bmiMassage;
+  document.getElementById("numberBMI").innerHTML = `${roundBMI}`;
 });
